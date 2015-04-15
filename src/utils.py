a46e5c5e244f727b8db94529a70bad930d4a96b8
Ilya Shamovsky <ilya.shamovsky@nyumc.org>
2015-04-15 13:22:35 -0400

    Use re for split_pool

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 import gzip
 import sys
+import os
+import re
 import pandas as pd
 from skbio.parse.sequences import parse_fastq
 from IPython.display import clear_output
@@ -17,6 +19,7 @@ def split_by_index(read1, read2, barcodes, bc_pos=(26,6)):
     # Read name line MUST start with @
     fastq_tpl = '@{id}\n{seq}\n+\n{q}\n' 
     cnt = 0
+    assigned = 0
     for rec1, rec2 in izip(parse_fastq(read1), parse_fastq(read2)):
         id1, seq1, q1 = rec1
         id2, seq2, q2 = rec2
@@ -26,7 +29,10 @@ def split_by_index(read1, read2, barcodes, bc_pos=(26,6)):
             sys.stdout.flush()
         istart, ilen = bc_pos
         ind = seq1[istart:istart+ilen]
+        # It's an exact match for now but we really need to accomodate
+        # mismatches here. Hamming distance?
         if ind in barcodes:
+            assigned += 1
             qstr1 = ''.join([chr(val+33) for val in q1])
             qstr2 = ''.join([chr(val+33) for val in q2])
             if not(ind in output_files):
@@ -38,6 +44,7 @@ def split_by_index(read1, read2, barcodes, bc_pos=(26,6)):
             output_files[ind][0].write(fastq_tpl.format(id=id1,seq=seq1,q=qstr1))
             output_files[ind][1].write(fastq_tpl.format(id=id2,seq=seq2,q=qstr2))
     print output_files.keys()
+    print 'Assigned:\t%d sequences' % assigned
     for ind,files in output_files.items():
         f1, f2 = files
         f1.close()
@@ -53,6 +60,8 @@ def split_pools(barcode, dirname='../data'):
     files_R1 = [os.path.join(dirname, f) for f in files if 'R1' in f]
     files_R2 = [os.path.join(dirname, f) for f in files if 'R2' in f]
     fastq_tpl='@{id}\n{seq}\n+\n{qual}\n'
+    minus_re = re.compile('^[CT][CT][CT][AG]')
+    plus_re = re.compile('^[AG][AG][AG][CT]')
     for file_R1,file_R2 in zip(files_R1, files_R2):
         print "Processing files:\t{f1}\t{f2}".format(f1=file_R1, f2=file_R2)
         sys.stdout.flush()
@@ -64,17 +73,19 @@ def split_pools(barcode, dirname='../data'):
              gzip.open(os.path.join(dirname, barcode+'_plus.fastq.gz'), 'wb') as gz_plus:
             for rec1,rec2 in izip(parse_fastq(gzr1), parse_fastq(gzr2)):
                 cnt += 1
-                if cnt % 100000 == 0:
+                if cnt % 1000000 == 0:
                     print "\t\t{0}\trecords...".format(cnt)
                     sys.stdout.flush()
                 id1, seq1, qual1 = rec1
                 id2, seq2, qual2 = rec2
                 qual_str = ''.join([chr(33+q) for q in qual2])
-                if seq1[0] in 'CTN' and seq1[1] in 'CTN' and seq1[2] in 'CTN' and seq1[3] in 'AGN':
+                if minus_re.match(seq1):
                     gz_minus.write(fastq_tpl.format(id=id2,seq=seq2,qual=qual_str))
                     cnt_minus += 1
-                elif seq1[0] in 'AGN' and seq1[1] in 'AGN' and seq1[2] in 'AGN' and seq1[3] in 'CTN':
+                elif plus_re.match(seq1):
                     gz_plus.write(fastq_tpl.format(id=id2,seq=seq2,qual=qual_str))
                     cnt_plus += 1
             print "{0}\tplus records\t{1}\tminus records".format(cnt_plus, cnt_minus)
             sys.stdout.flush()
+            
+
